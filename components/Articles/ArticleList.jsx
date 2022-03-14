15a7868d23c7a4b621fb235edccd3bef69ef30e0
Rufas Legeckas <legeckasrufas@gmail.com>
2022-03-15 00:43:27 +0200

    removing files and adjusting gitignore

--- components/Articles/ArticleList.jsx
+++
@@ -1,8 +0,0 @@
-import articleStyles from './Article.module.scss'
-const ArticleList = ({articles}) => {
-    return (
-        <div className={articleStyles.grid}>
-            {articles.map(articles => <h3>{articles.title}</h3>)}
-        </div>
-    )
-}
--- components/Blog/index.jsx
+++
@@ -1,8 +0,0 @@
-const Vardas = () => {
-    return (
-        <div>
-            <div>tekstas</div>
-        </div>
-    )
-}
-export default Vardas;
