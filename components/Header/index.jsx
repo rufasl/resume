f4feeba6fd1365f76f1a73a3f88f204a07c6adcc
Rufas Legeckas <legeckasrufas@gmail.com>
2022-03-23 01:33:38 +0200

    adjusting how title is saved to homepage

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -4,7 +4,7 @@ import BtnStyles from "../Button/button.module.css";
 export default function Header() {
     return (
         <div className={headerStyles.fontName}>
-            <title>sss</title>
+          <title>Welcome - Home</title>
             <meta name='keywords' content='web development, programming, e-commerce'></meta>
             <div>
                 <div className={headerStyles.image}>
