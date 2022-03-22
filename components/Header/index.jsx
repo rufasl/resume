3873f9aa8b460997ac563808d3c185747a50438d
Rufas Legeckas <legeckasrufas@gmail.com>
2022-03-23 00:50:52 +0200

    adjusting content and styles for the pages

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -7,9 +7,12 @@ export default function Header() {
             <title>Blog</title>
             <meta name='keywords' content='web development, programming, e-commerce'></meta>
             <div>
+                <div className={headerStyles.image}>
+                    <a href="/about"><img src='https://media-exp1.licdn.com/dms/image/C4D03AQGMxkjXHcUITA/profile-displayphoto-shrink_800_800/0/1630486542215?e=1652918400&v=beta&t=iAKhGvnKydInRUtVp3XBjQivI_NkQ-65aVfZGPUR-cw'/></a>
+                </div>
                 <p className={headerStyles.heading}>Rufas Legeckas,</p>
                 <p className={headerStyles.heading2}>Project manager / Team Lead</p>
-                <p className={headerStyles.descriptionText}>This website is done on Next.js from scratch by me</p>
+                <p className={headerStyles.descriptionText}>This website is done on Next.js from scratch by me, <i>sorry had to flex</i></p>
                 <form action='/about'>
                     <button type="submit" className={BtnStyles.button}><span>Resume</span></button>
                 </form>
--- pages/about.jsx
+++ pages/about.jsx
@@ -3,38 +3,40 @@ import aboutStyles from '../components/About/About.module.scss'
 export default function About() {
     return (
         <div>
-            <title>Who is this guy?</title>
+            <title>Who is this guy? - Resume</title>
             <div>
-                <h1>Nice, you clicked, I should get some analytics traffic.</h1>
-                <div>
-                    <img src='https://media-exp1.licdn.com/dms/image/C4D03AQGMxkjXHcUITA/profile-displayphoto-shrink_800_800/0/1630486542215?e=1652918400&v=beta&t=iAKhGvnKydInRUtVp3XBjQivI_NkQ-65aVfZGPUR-cw'/>
+                <div className={aboutStyles.center}>
+                    <h2>Hey, you clicked, I should get some analytics traffic.</h2>
                 </div>
-                <h2>Hi,</h2>
-                <h2>My name is Rufas.</h2>
+                <p>I'm Rufas Legeckas, currently from Panevėžys city, Lithuania.</p>
                 <h3>Skill tree:</h3>
                 <ul>
-                    <li>Obviously insane programming skills as you can see from this website - <strong>bachelors in Computer Science</strong></li>
+                    <li>Team Lead for a development team of 10 and their monthly personal and team goals, personal and team results, invoices</li>
                     <li>Project management experience developing Magento 2 webshops and WordPress websites - <strong>5 years at Creation Labs (UAB Elsoft)</strong></li>
-                    <li>Responsible as a Team Lead for a development team of 10 and their monthly personal and team goals, personal and team results, invoices</li>
+                    <li><i>Obviously</i> insane programming skills as you can see from this website - <i>website is built on next.js/node.js frameworks only by me</i> <strong>bachelors in Computer Science 2017, Panevėžys</strong></li>
                     <li>Wit and English skills - <strong>in communication with partners abroad</strong></li>
-                    <li>Great looks - <strong>should probably workout though</strong></li>
-                    <li>Tends to think he can do almost everything in this life, except for maybe making this page look appealing</li>
-                    <li>Level 99 magic</li>
+                    <li>I speak fluent in nerd, meaining communication with the people responsible for excecuting the tasks is easy</li>
+                    <li>Tend to think I can do almost everything in life, except for maybe making this page look appealing, <i>apparently</i></li>
                 </ul>
-                <h3>Projects</h3>
+                <h3>Projects/responsibilities</h3>
                 <ul>
-                    <li><a href='https://lemodus.lt'>Lemodus.lt Magento 2 webshop with integrations with clients resources, custom functionality</a></li>
-                    <li>Many WordPress projects</li>
+                    <li>Responsible for technical decisions and management for custom design project <a href='https://lemodus.lt'>Lemodus.lt</a> Magento 2 webshop with integrations with clients resources, custom functionality.</li>
+                    <li><a href='https://www.leatherlook.nl/'>Leatherlook project</a> custom desingn made on Magento 2, working with outsourced developers and assigning tasks, managing budget etc.</li>
+                    <li>Working with <a href='https://juist.nl/'>development partners abroad</a> and managing the team working on Magento 2 support/Wordpress/Testing, fixing and implementing procedures, assigning responsabilities to the team</li>
+                    <li>Team Lead responsability - growth and competence evaluations held quarterly, discussing growth and making decisions if the developer/manager/tester is ready to level up. Daily meetings, discussing technical decisions, support for colleagues etc.</li>
+                    <li>Writing project documentations and making project estimations as project manager, so that the terms and agreements make sense to client and developer</li>
+                    <li>Monitoring team performance hours spent/billed to client, adjusting workflow and making employment decisions</li>
+                    <li>Many other Magento 1 and 2, WordPress projects and support</li>
                 </ul>
                 <div>
                     <h3>Goals</h3>
-                    <p>I consider myself a goal oriented person, so I tend to make goals and work toward them.</p>
-                    <p>The goals now are as follows:</p>
                     <ul>
-                        <li>Make blog content and publish the page</li>
                         <li>Get a gym membership</li>
                         <li className={aboutStyles.level2}>Actually attend the gym</li>
-                        <li>Learn React/Next.js by developing this website</li>
+                        <li>Learn more headless programming and Next.js/Node.js by developing this website</li>
+                        <li className={aboutStyles.level2}>Develop a blog page and write posts</li>
+                        <li className={aboutStyles.level2}>Learn more about anaytics while monitoring the blog's results</li>
+                        <li>Find a workplace I can vibe with and learn more geeky stuff</li>
                     </ul>
                 </div>
             </div>
